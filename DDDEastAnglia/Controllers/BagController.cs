e509a326c9007b12f03c70f99d231f984fe64eb6
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-06-20 19:01:47 +0100

    Created BagController, IndexViewModel and Index view

---
+++ DDDEastAnglia/Controllers/BagController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DDDEastAnglia.Controllers
+{
+    public class BagController : Controller
+    {
+        // GET
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
