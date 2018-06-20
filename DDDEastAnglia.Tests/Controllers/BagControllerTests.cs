4273e713fc40661de1c5867bb54b1a208da4e82e
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-06-20 19:02:06 +0100

    Initial test for BagController

---
+++ DDDEastAnglia.Tests/Controllers/BagControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using DDDEastAnglia.Controllers;
+using DDDEastAnglia.Models;
+using NUnit.Framework;
+
+namespace DDDEastAnglia.Tests.Controllers
+{
+
+    [TestFixture]
+    public class BagControllerTests
+    {
+        [Test]
+        public void Order_Number_Not_For_DDDEA_Returns_Same_View()
+        {
+            BagController controller = new BagController();
+            BagIndexViewModel model = new BagIndexViewModel();
+            var result = controller.Index();
+            Assert.That(((ViewResult)result).ViewName, Is.EqualTo("Index"));
+        }
+    }
+}
