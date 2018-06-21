b45a98089910797b59b7a033a9bc635b00791b0b
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-06-21 12:14:56 +0100

    Refactored out creation of mock to a SetUp method

--- DDDEastAnglia.Tests/Controllers/BagControllerTests.cs
+++ DDDEastAnglia.Tests/Controllers/BagControllerTests.cs
@@ -7,14 +7,20 @@ using NUnit.Framework;
 
 namespace DDDEastAnglia.Tests.Controllers
 {
-
     [TestFixture]
     public class BagControllerTests
     {
+        private ITicketProvider ticketProvider;
+
+        [SetUp]
+        public void Setup()
+        {
+            ticketProvider = Substitute.For<ITicketProvider>();
+        }
+
         [Test]
         public void Order_Number_Not_For_DDDEA_Returns_Same_View()
         {
-            ITicketProvider ticketProvider = Substitute.For<ITicketProvider>();
             ticketProvider.TicketIsForOurEvent("1234567890").Returns(false);
             BagController controller = new BagController(ticketProvider);
             BagIndexViewModel model = new BagIndexViewModel {OrderNumber = "1234567890"};
@@ -25,13 +31,11 @@ namespace DDDEastAnglia.Tests.Controllers
         [Test]
         public void Order_Number_For_DDDEA_Returns_Contents_View()
         {
-            ITicketProvider ticketProvider = Substitute.For<ITicketProvider>();
             ticketProvider.TicketIsForOurEvent("1234567890").Returns(true);
             BagController controller = new BagController(ticketProvider);
             BagIndexViewModel model = new BagIndexViewModel { OrderNumber = "1234567890" };
             var result = controller.Index(model);
             Assert.That(result, Is.TypeOf<RedirectToRouteResult>());
         }
-
     }
 }
