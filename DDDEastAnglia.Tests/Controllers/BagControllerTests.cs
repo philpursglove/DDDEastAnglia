43111cb5ff1bff59a56c0e291aab5d021c969efe
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-06-21 16:03:10 +0100

    Regfactor controller creation out to a method

--- DDDEastAnglia.Tests/Controllers/BagControllerTests.cs
+++ DDDEastAnglia.Tests/Controllers/BagControllerTests.cs
@@ -30,21 +30,25 @@ namespace DDDEastAnglia.Tests.Controllers
         public void Order_Number_Not_For_DDDEA_Returns_Same_View()
         {
             ticketProvider.TicketIsForOurEvent("1234567890").Returns(false);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             BagIndexViewModel model = new BagIndexViewModel {OrderNumber = "1234567890"};
             var result = controller.Index(model);
             Assert.That(((ViewResult)result).ViewName, Is.EqualTo("Index"));
+        }
+
+        private BagController CreateController()
+        {
+            BagController controller = new BagController(ticketProvider);
+            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
+            controller.ControllerContext = controllerContext;
+            return controller;
         }
 
         [Test]
         public void Order_Number_For_DDDEA_Returns_Contents_View()
         {
             ticketProvider.TicketIsForOurEvent("1234567890").Returns(true);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             BagIndexViewModel model = new BagIndexViewModel { OrderNumber = "1234567890" };
             var result = controller.Index(model);
             Assert.That(result, Is.TypeOf<RedirectToRouteResult>());
@@ -54,9 +58,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void Contents_View_Returns_BagContents()
         {
             sessionStateBase["ValidatedTicket"].Returns(true);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             ViewResult result = (ViewResult)controller.Contents();
             Assert.That(result.Model, Is.TypeOf<BagContentsViewModel>());
         }
@@ -65,9 +67,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void When_The_User_Has_Already_Validated_Their_Ticket_Pass_Them_To_The_Contents_Action()
         {
             sessionStateBase["ValidatedTicket"].Returns(true);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             var result = controller.Index();
             Assert.That(result, Is.TypeOf<RedirectToRouteResult>());
         }
@@ -76,9 +76,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void When_The_User_Has_Not_Validated_Their_Ticket_Return_Them_To_The_Index_View()
         {
             sessionStateBase["ValidatedTicket"].Returns(false);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             var result = controller.Contents();
             Assert.That(result, Is.TypeOf<RedirectToRouteResult>());
         }
@@ -87,9 +85,7 @@ namespace DDDEastAnglia.Tests.Controllers
         public void Order_Number_For_DDDEA_Sets_The_SessionState()
         {
             ticketProvider.TicketIsForOurEvent("1234567890").Returns(true);
-            BagController controller = new BagController(ticketProvider);
-            ControllerContext controllerContext = new ControllerContext(contextBase, new RouteData(), controller);
-            controller.ControllerContext = controllerContext;
+            BagController controller = CreateController();
             BagIndexViewModel model = new BagIndexViewModel { OrderNumber = "1234567890" };
             controller.Index(model);
             Assert.That(sessionStateBase["ValidatedTicket"], Is.True);
