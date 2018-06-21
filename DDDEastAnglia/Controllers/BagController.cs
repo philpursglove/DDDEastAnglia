dd4a1f7631a3d976eef53828fb3d269866f491c0
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-06-21 12:06:20 +0100

    BagCOntroller uses ITicketProvider to determine if the submitted number was for our event

--- DDDEastAnglia/Controllers/BagController.cs
+++ DDDEastAnglia/Controllers/BagController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Web.Mvc;
+using DDDEastAnglia.Helpers;
 using DDDEastAnglia.Models;
 
 namespace DDDEastAnglia.Controllers
 {
     public class BagController : Controller
     {
+        private readonly ITicketProvider ticketProvider;
+
+        public BagController(ITicketProvider ticketProvider)
+        {
+            this.ticketProvider = ticketProvider ?? throw new System.ArgumentNullException(nameof(ticketProvider));
+        }
         // GET
         public ActionResult Index()
         {
@@ -14,7 +21,16 @@ namespace DDDEastAnglia.Controllers
         [HttpPost]
         public ActionResult Index(BagIndexViewModel model)
         {
+            if (ticketProvider.TicketIsForOurEvent(model.OrderNumber))
+            {
+                return RedirectToAction("Contents");
+            }
             return View("Index");
+        }
+
+        public ActionResult Contents()
+        {
+            return View();
         }
     }
 }
