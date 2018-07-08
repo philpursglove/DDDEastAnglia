da6a773b8efed71216d1e8432351e23cedd2a93b
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-07-08 22:12:43 +0100

    Use the Eventbrite OAuth token; use hashing instead of GetHashCode

--- DDDEastAnglia/Helpers/EventbriteTicketProvider.cs
+++ DDDEastAnglia/Helpers/EventbriteTicketProvider.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Security.Cryptography;
+using System.Text;
 using DDDEastAnglia.Helpers.AppSettings;
 using Newtonsoft.Json.Linq;
 
@@ -8,7 +10,8 @@ namespace DDDEastAnglia.Helpers
     {
         public bool TicketIsForOurEvent(string ticketNumber)
         {
-            string eventBriteApiKey = new WebConfigurationAppSettingsProvider().GetSetting("EventbriteAPIKey");
+            string eventBriteApiKey = new WebConfigurationAppSettingsProvider().GetSetting("EventbriteOAuthToken");
+            string eventHashValue = "1c67b45e0cb0f83e2e5f39ca9261701edabb36ea24c0125bf11d477600dcbe1e";
             using (WebClient webClient = new WebClient())
             {
                 ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -26,7 +29,19 @@ namespace DDDEastAnglia.Helpers
                     eventBriteApiKey));
                 JToken eventObject = JToken.Parse(eventJson);
                 string eventName = eventObject.SelectToken("name.text").Value<string>();
-                if (eventName.ToLowerInvariant().StartsWith("ddd east anglia"))
+                eventName = eventName.Substring(0, 15).ToLowerInvariant();
+
+                byte[] eventNameBytes = Encoding.Unicode.GetBytes(eventName);
+                HashAlgorithm sha256 = new SHA256CryptoServiceProvider();
+                byte[] hashedEventNameBytes = sha256.ComputeHash(eventNameBytes);
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashedEventNameBytes.Length; i++)
+                {
+                    builder.Append(hashedEventNameBytes[i].ToString("x2"));
+                }
+                string hashedEventName = builder.ToString();
+
+                if (hashedEventName == eventHashValue)
                 {
                     return true;
                 }
