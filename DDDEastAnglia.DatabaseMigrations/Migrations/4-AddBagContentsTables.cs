c4578ccc7fed91b72baa81fd52ec0752ba61b983
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-08-02 22:41:45 +0100

    Database objects for Bag and files

---
+++ DDDEastAnglia.DatabaseMigrations/Migrations/4-AddBagContentsTables.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace DDDEastAnglia.DatabaseMigrations.Migrations
+{
+    public class AddBagContentsTables : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("BagContents")
+                .WithColumn("ContentId").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("SponsorId").AsInt32().NotNullable()
+                .WithColumn("ContentText").AsString(int.MaxValue);
+
+            Create.Table("BagFiles")
+                .WithColumn("BagFileId").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("ContentId").AsInt32().NotNullable()
+                .WithColumn("Filename").AsString().NotNullable()
+                .WithColumn("FileBytes").AsBinary(int.MaxValue).NotNullable()
+                .WithColumn("FileMIMEType").AsString(int.MaxValue).NotNullable()
+                .WithColumn("FileLength").AsInt32().NotNullable();
+        }
+    }
+}
