e7297a7fb88278fb01f08b6c7fd0cd8b865fa308
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-08-03 09:37:47 +0100

    Initial setup for admin bag ui

---
+++ DDDEastAnglia/Areas/Admin/Controllers/BagController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using DDDEastAnglia.Models;
+
+namespace DDDEastAnglia.Areas.Admin.Controllers
+{
+    public class BagController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            
+        }
+
+        [HttpPost]
+        public ActionResult Edit(SponsorBagContent model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            
+        }
+    }
+}
