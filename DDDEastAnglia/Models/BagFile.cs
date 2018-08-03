2c79fff4058f385d80bb0fa6b11563e7161702b4
Phil Pursglove <philpursglove@users.noreply.github.com>
2018-08-03 09:38:09 +0100

    Models for bag things

---
+++ DDDEastAnglia/Models/BagFile.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DDDEastAnglia.Models
+{
+    public class BagFile
+    {
+        [Key]
+        public int BagFileId { get; set; }
+
+        public int ContentId { get; set; }
+
+        public string Filename { get; set; }
+
+        public string MIMEType { get; set; }
+
+        public byte[] Bytes { get; set; }
+
+        public int FileLength { get; set; }
+    }
+}
--- DDDEastAnglia/Models/SponsorBagContent.cs
+++ DDDEastAnglia/Models/SponsorBagContent.cs
@@ -1,7 +1,14 @@
-﻿namespace DDDEastAnglia.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DDDEastAnglia.Models
 {
     public class SponsorBagContent
     {
+        [Key]
+        public int ContentId { get; set; }
 
+        public int SponsorId { get; set; }
+
+        public string ContentText { get; set; }
     }
 }
